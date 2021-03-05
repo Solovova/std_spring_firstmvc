cd667c9e2fb3d5b74b5f5fb1fecea69b280d44ff
Solovova <vsolo0@gmail.com>
2021-03-05 13:26:32 +0200

    0.08

--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 https://www.youtube.com/watch?v=BgE5DoIN6Bs&list=PLAma_mKffTOR5o0WNHnY0mTjKxnCgSXrZ&index=15&ab_channel=alishev
 
 https://start.spring.io/
+
+https://www.youtube.com/watch?v=JHTqKQgrVKE&list=PLAma_mKffTOR5o0WNHnY0mTjKxnCgSXrZ&index=18&ab_channel=alishev
---
+++ src/main/kotlin/org/solovova/std_spring_firstmvc/controllers/FirstController.kt
@@ -0,0 +1,18 @@
+package org.solovova.std_spring_firstmvc.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+
+@Controller
+class FirstController {
+   @GetMapping("/hello")
+    fun pageHello(): String {
+        return "first/hello"
+    }
+
+    @GetMapping("/goodbye")
+    fun pageGoodBye(): String {
+        return "first/goodbye"
+    }
+}
