b8d9c22863e34596034b014988baf4bc283adee7
Solovova <vsolo0@gmail.com>
2021-03-05 11:27:36 +0200

    0.06

--- src/main/kotlin/org/solovova/std_spring_firstmvc/HelloController.kt
+++ src/main/kotlin/org/solovova/std_spring_firstmvc/HelloController.kt
@@ -2,11 +2,14 @@ package org.solovova.std_spring_firstmvc
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 
 @Controller
 class HelloController {
-    @GetMapping("/hello-world")
+   // @GetMapping("/hello-world")
+   @GetMapping("/hello")
     fun sayHello(): String {
         return "hello_world"
     }
