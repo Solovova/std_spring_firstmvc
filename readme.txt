cca0e695dba8af31e7ee5a033e90a87bd3ea646b
Solovova <vsolo0@gmail.com>
2021-03-06 17:51:20 +0200

    0.11

--- readme.txt
+++ readme.txt
@@ -3,3 +3,4 @@ https://www.youtube.com/watch?v=BgE5DoIN6Bs&list=PLAma_mKffTOR5o0WNHnY0mTjKxnCgS
 https://start.spring.io/
 
 https://www.youtube.com/watch?v=JHTqKQgrVKE&list=PLAma_mKffTOR5o0WNHnY0mTjKxnCgSXrZ&index=18&ab_channel=alishev
+https://www.youtube.com/watch?v=S8QgGX8HA9k&list=PLAma_mKffTOR5o0WNHnY0mTjKxnCgSXrZ&index=21&ab_channel=alishev
--- src/main/kotlin/org/solovova/std_spring_firstmvc/controllers/FirstController.kt
+++ src/main/kotlin/org/solovova/std_spring_firstmvc/controllers/FirstController.kt
@@ -2,18 +2,23 @@ package org.solovova.std_spring_firstmvc.controllers
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.http.HttpServletRequest
 
 
 @Controller
 //@RequestMapping("/first") //  /first/hello
 class FirstController {
-   @GetMapping("/hello")
-    fun pageHello(): String {
+    @GetMapping("/hello")
+    fun pageHello(request: HttpServletRequest): String {
+        val name = request.getParameter("name")
+        println(name)
         return "first/hello"
     }
 
     @GetMapping("/goodbye")
-    fun pageGoodBye(): String {
+    fun pageGoodBye(@RequestParam(defaultValue = "", required = false, name = "name") name1: String): String {
+        println(name1)
         return "first/goodbye"
     }
 }
